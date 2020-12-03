37e0d7d0b4eeb5dab83cfb5fc00ef24d98f11cc1
Joaquim Oliveira <joaquim.oliveira.nt@gmail.com>
2020-12-03 16:25:51 -0300

    this is it

--- app_test.py
+++ app_test.py
@@ -17,12 +17,10 @@ from rogue.lib import get_recommendations, get_users_ratings_df
 from rogue.sim_model import SimModel
 import time
 
-
 @st.cache(suppress_st_warning=True)
 def get_dfs():
     BUCKET_NAME = 'rogue-data'
     BUCKET_PATH = f"gs://{BUCKET_NAME}/raw_data"
-
 
     # df_content = pd.read_csv('raw_data/streamlit-data/soup_lite.csv')
     # df_rating = pd.read_csv('raw_data/streamlit-data/ratings_lite.csv')
@@ -36,11 +34,9 @@ def get_dfs():
     df_rating = df_rating.pivot(
         index='userId', columns='title', values='rating')
 
-    return df_content, df_rating, df_rating_c
+    return df_content , df_rating ,df_rating_c
 
-
-df_content, df_rating, df_rating_c = get_dfs()
-
+df_content , df_rating, df_rating_c = get_dfs()
 
 @st.cache(suppress_st_warning=True)
 def get_sim(soup):
@@ -48,11 +44,10 @@ def get_sim(soup):
     similarity = SimModel.get_similarity(matrix)
     return model
 
-
 def main():
     state = _get_state()
     pages = {
-        "Home": page_home,
+        "Home" : page_home,
         "Content": page_content,
         "Rating": page_rating,
     }
@@ -65,7 +60,6 @@ def main():
 
     # Mandatory to avoid rollbacks with widgets, must be called at the end of your app
     state.sync()
-
 
 def page_home(state):
     st.image('streamlit-images/header.png')
@@ -83,17 +77,14 @@ def page_content(state):
 
     filmnames = df_content['title'].values.tolist()
 
-    options = st.multiselect(
-        'Which movie would you like to compare?', filmnames)
+    options = st.multiselect('Which movie would you like to compare?',filmnames)
 
     if options is not None:
         for i in options:
-            recommendation = SimModel.get_recommendations(
-                i, similarity, df_content)
+            recommendation = SimModel.get_recommendations(i,similarity, df_content)
             st.markdown("""## This is the film you chose""")
             st.header(recommendation.iloc[0].title)
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommendation.iloc[0].poster_path}", width=170, use_column_width=False)
+            st.image(f"https://image.tmdb.org/t/p/original/{recommendation.iloc[0].poster_path}", width=170, use_column_width=False)
             st.markdown('''## Sorty by:''')
             col1, col2 = st.beta_columns(2)
             with col1:
@@ -114,48 +105,39 @@ def page_content(state):
                     #### a number given by:
                     - *The sum of all votes given to a film divided by the number of votes*
                     ''')
-            options_sort = st.multiselect(
-                'Select', ['popularity', 'vote_average'])
+            options_sort = st.multiselect('Select',['popularity', 'vote_average'])
             if options_sort is not None:
                 for i in options_sort:
-                    recommendation_2 = recommendation.copy().sort_values(
-                        by=i, ascending=False).drop([0])
+                    recommendation_2 = recommendation.copy().sort_values(by=i, ascending=False).drop([0])
 
                     st.markdown("""## This are some films you might like""")
                     col1, col2, col3, col4 = st.beta_columns(4)
                     with col1:
-                        caption = recommendation_2.iloc[1].title
-                        st.image(
-                            f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[1].poster_path}", use_column_width=True, caption=caption)
+                        caption=recommendation_2.iloc[1].title
+                        st.image(f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[1].poster_path}", use_column_width=True , caption=caption)
                     with col2:
-                        caption = recommendation_2.iloc[2].title
-                        st.image(
-                            f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[2].poster_path}", use_column_width=True, caption=caption)
+                        caption=recommendation_2.iloc[2].title
+                        st.image(f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[2].poster_path}", use_column_width=True, caption=caption)
                     with col3:
-                        caption = recommendation_2.iloc[3].title
-                        st.image(
-                            f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[3].poster_path}", use_column_width=True, caption=caption)
+                        caption=recommendation_2.iloc[3].title
+                        st.image(f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[3].poster_path}", use_column_width=True, caption=caption)
                     with col4:
-                        caption = recommendation_2.iloc[4].title
-                        st.image(
-                            f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[4].poster_path}", use_column_width=True, caption=caption)
+                        caption=recommendation_2.iloc[4].title
+                        st.image(f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[4].poster_path}", use_column_width=True, caption=caption)
                     col1, col2, col3, col4 = st.beta_columns(4)
                     with col1:
-                        caption = recommendation_2.iloc[5].title
-                        st.image(
-                            f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[5].poster_path}", use_column_width=True, caption=caption)
+                        caption=recommendation_2.iloc[5].title
+                        st.image(f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[5].poster_path}", use_column_width=True , caption=caption)
                     with col2:
-                        caption = recommendation_2.iloc[6].title
-                        st.image(
-                            f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[6].poster_path}", use_column_width=True, caption=caption)
+                        caption=recommendation_2.iloc[6].title
+                        st.image(f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[6].poster_path}", use_column_width=True, caption=caption)
                     with col3:
-                        caption = recommendation_2.iloc[7].title
-                        st.image(
-                            f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[7].poster_path}", use_column_width=True, caption=caption)
+                        caption=recommendation_2.iloc[7].title
+                        st.image(f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[7].poster_path}", use_column_width=True, caption=caption)
                     with col4:
-                        caption = recommendation_2.iloc[8].title
-                        st.image(
-                            f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[8].poster_path}", use_column_width=True, caption=caption)
+                        caption=recommendation_2.iloc[8].title
+                        st.image(f"https://image.tmdb.org/t/p/original/{recommendation_2.iloc[8].poster_path}", use_column_width=True, caption=caption)
+
 
 
 def page_rating(state):
@@ -163,18 +145,20 @@ def page_rating(state):
 
     @st.cache(suppress_st_warning=True)
     def get_random_subset(df_content):
-        random_df = df_content[0:20].sort_values(
-            by='total_ratings', ascending=False)
+        random_df = df_content[0:20].sort_values(by='total_ratings', ascending=False)
         random_df = df_content.sample(10)
         return random_df
 
+
     random_df = get_random_subset(df_rating_c)
     movies_to_rate = random_df['title'].values.tolist()
+
 
     key = 0
     movid = 0
     user_ratings = []
     col = 0
+
 
     ####### RATE FILMS #######
 
@@ -183,19 +167,19 @@ def page_rating(state):
         while key <= 3:
             with cols1[col]:
                 poster = f"https://image.tmdb.org/t/p/original/{random_df.iloc[movid].poster_path}"
-                caption = random_df.iloc[movid].title
-                cols1[col].image(poster, width=150, caption=caption)
-                if st.button('Never Seen', key=key):
+                caption=random_df.iloc[movid].title
+                cols1[col].image(poster, width=150,caption=caption)
+                if  st.button('Never Seen' , key=key):
                     pass
                 else:
-                    vote = cols1[col].number_input(
-                        'Give a Rating', key=key, min_value=1, max_value=5)
+                    vote = cols1[col].number_input('Give a Rating', key =key, min_value=1,max_value=5)
                     user_ratings.append(vote)
                 # vote = cols1[col].number_input('Give a Rating', key =key, min_value=1,max_value=5)
                 # user_ratings.append(vote)
                 col += 1
                 key += 1
                 movid += 1
+
 
     key = 4
     cols2 = st.beta_columns(4)
@@ -204,13 +188,12 @@ def page_rating(state):
         while key <= 7:
             with cols2[col]:
                 poster = f"https://image.tmdb.org/t/p/original/{random_df.iloc[movid].poster_path}"
-                caption = random_df.iloc[movid].title
-                cols2[col].image(poster, width=150, caption=caption)
-                if st.button('Never Seen', key=key):
+                caption=random_df.iloc[movid].title
+                cols2[col].image(poster, width=150,caption=caption)
+                if  st.button('Never Seen' , key=key):
                     pass
                 else:
-                    vote = cols2[col].number_input(
-                        'Give a Rating', key=key, min_value=1, max_value=5)
+                    vote = cols2[col].number_input('Give a Rating', key =key, min_value=1,max_value=5)
                     user_ratings.append(vote)
                 # vote = cols2[col].number_input('Give a Rating', key =key, min_value=1,max_value=5)
                 # user_ratings.append(vote)
@@ -225,13 +208,12 @@ def page_rating(state):
         while key <= 9:
             with cols3[col]:
                 poster = f"https://image.tmdb.org/t/p/original/{random_df.iloc[movid].poster_path}"
-                caption = random_df.iloc[movid].title
-                cols3[col].image(poster, width=150, caption=caption)
-                if st.button('Never Seen', key=key):
+                caption=random_df.iloc[movid].title
+                cols3[col].image(poster, width=150,caption=caption)
+                if  st.button('Never Seen' , key=key):
                     pass
                 else:
-                    vote = cols3[col].number_input(
-                        'Give a Rating', key=key, min_value=1, max_value=5)
+                    vote = cols3[col].number_input('Give a Rating', key =key, min_value=1,max_value=5)
                     user_ratings.append(vote)
                 # vote = cols3[col].number_input('Give a Rating', key =key, min_value=1,max_value=5)
                 # user_ratings.append(vote)
@@ -241,7 +223,11 @@ def page_rating(state):
 
     ####### PREDICT #######
 
+
+
+
     if st.button('SUBMIT'):
+
 
         my_bar = st.progress(0)
         for percent_complete in range(100):
@@ -257,46 +243,37 @@ def page_rating(state):
         st.markdown("""## This are some films you might like""")
         col1, col2, col3, col4, col5 = st.beta_columns(5)
         with col1:
-            caption = recommended_movies.iloc[0].title
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[0].poster_path}", use_column_width=True, caption=caption)
+            caption=recommended_movies.iloc[0].title
+            st.image(f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[0].poster_path}", use_column_width=True , caption=caption)
         with col2:
-            caption = recommended_movies.iloc[1].title
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[1].poster_path}", use_column_width=True, caption=caption)
+            caption=recommended_movies.iloc[1].title
+            st.image(f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[1].poster_path}", use_column_width=True, caption=caption)
         with col3:
-            caption = recommended_movies.iloc[2].title
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[2].poster_path}", use_column_width=True, caption=caption)
+            caption=recommended_movies.iloc[2].title
+            st.image(f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[2].poster_path}", use_column_width=True, caption=caption)
         with col4:
-            caption = recommended_movies.iloc[3].title
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[3].poster_path}", use_column_width=True, caption=caption)
+            caption=recommended_movies.iloc[3].title
+            st.image(f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[3].poster_path}", use_column_width=True, caption=caption)
         with col5:
-            caption = recommended_movies.iloc[4].title
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[4].poster_path}", use_column_width=True, caption=caption)
+            caption=recommended_movies.iloc[4].title
+            st.image(f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[4].poster_path}", use_column_width=True , caption=caption)
         col1, col2, col3, col4, col5 = st.beta_columns(5)
         with col1:
-            caption = recommended_movies.iloc[5].title
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[5].poster_path}", use_column_width=True, caption=caption)
+            caption=recommended_movies.iloc[5].title
+            st.image(f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[5].poster_path}", use_column_width=True , caption=caption)
         with col2:
-            caption = recommended_movies.iloc[6].title
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[6].poster_path}", use_column_width=True, caption=caption)
+            caption=recommended_movies.iloc[6].title
+            st.image(f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[6].poster_path}", use_column_width=True, caption=caption)
         with col3:
-            caption = recommended_movies.iloc[7].title
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[7].poster_path}", use_column_width=True, caption=caption)
+            caption=recommended_movies.iloc[7].title
+            st.image(f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[7].poster_path}", use_column_width=True, caption=caption)
         with col4:
-            caption = recommended_movies.iloc[8].title
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[8].poster_path}", use_column_width=True, caption=caption)
+            caption=recommended_movies.iloc[8].title
+            st.image(f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[8].poster_path}", use_column_width=True, caption=caption)
         with col5:
-            caption = recommended_movies.iloc[9].title
-            st.image(
-                f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[9].poster_path}", use_column_width=True, caption=caption)
+            caption=recommended_movies.iloc[9].title
+            st.image(f"https://image.tmdb.org/t/p/original/{recommended_movies.iloc[9].poster_path}", use_column_width=True, caption=caption)
+
 
 
 class _SessionState:
@@ -331,8 +308,7 @@ class _SessionState:
                 self._state["is_rerun"] = True
                 self._state["session"].request_rerun()
 
-        self._state["hash"] = self._state["hasher"].to_bytes(
-            self._state["data"], None)
+        self._state["hash"] = self._state["hasher"].to_bytes(self._state["data"], None)
 
 
 def _get_session():
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"${roger.rendon.guajardo@gmail.com}\"\n\
+email = \"${HEROKU_EMAIL_ADDRESS}\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
